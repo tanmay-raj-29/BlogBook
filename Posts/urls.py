dcbe7f20b1304c82df86225dd5cdfe7e1ff9fe90
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2021-07-17 18:37:33 +0530

    added login/logout functionality

--- Posts/urls.py
+++ Posts/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('features', views.feature, name='feature'),
     path('post/<str:slug>', views.post, name='post'),
     path('search', views.search, name='search'),
+    path('signup', views.signup, name='signup'),
+    path('login', views.handlelogin, name='handlelogin'),
+    path('logout', views.handlelogout, name='handlelogout')
 ]
--- Posts/views.py
+++ Posts/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Post, Feature
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+
 # Create your views here.
 
 def index(request):
@@ -25,7 +30,7 @@ def feature(request):
 
 def search(request):
     search = request.GET['search']
-    if len(search) > 50:
+    if len(search) > 50 or len(search) == 0:
         posts = []
     else:
         posts = Post.objects.filter(title__icontains=search)
@@ -34,3 +39,40 @@ def search(request):
         'search': search
     }
     return render(request, 'search.html', context)
+
+def signup(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+        if len(username) > 15:
+            messages.error(request, 'Your username must be under 15 characters')
+            return redirect('/')
+        if password != password1:
+            messages.error(request, 'passwords donot match')
+            return redirect('/')            
+        myUser = User.objects.create_user(username, email, password)
+        myUser.save()
+        login(request, myUser)
+        return redirect('/')
+    else:
+        return HttpResponse('404 - Not Found')
+
+def handlelogin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            messages.error(request, 'Invalid credentials. Please try again')
+            return redirect('/')
+    else:
+        return HttpResponse('404 - Not Found')
+
+def handlelogout(request):
+    logout(request)
+    return redirect('/')
